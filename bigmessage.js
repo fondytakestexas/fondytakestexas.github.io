498840b9f66f6c249084a0093bd1aa9b55a3e617
Maximilian Holzmann <max@holzmann.io>
2018-03-31 02:08:18 -0500

    test

--- bigmessage.js
+++
@@ -1,6 +0,0 @@
- function Message() {
-  event.preventDefault();
-  var text = document.getElementById('textbox').value;
-  var replacing = document.getElementById('textreplace');
-  replacing.innerText = text;
-}
